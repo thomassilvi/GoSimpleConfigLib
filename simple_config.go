4e317e2a23f570bb6dad94296df06d2cac6306ca
Thomas Silvi <thomas.silvi@laposte.net>
2014-02-25 15:50:54 +0100

    write string ok

--- simple_config.go
+++ simple_config.go
@@ -1,19 +1,77 @@
 package simple_config
 
 import (
+	"errors"
 	"fmt"
+	"os"
+	"reflect"
+	"strconv"
 )
 
-//-------------------------------------------------------------------------------------------------
+//---------------------------------------------------------------------------------------------------------------------
 
-func SimpleConfigRead(filename string) {
+func ReadConfig(filename string) (error) {
 	fmt.Println("TODO")
+
+	return nil
 }
 
-//-------------------------------------------------------------------------------------------------
+//---------------------------------------------------------------------------------------------------------------------
 
-func SimpleConfigWrite(filename string) {
-	fmt.Println("TODO")
+func WriteConfig(filename string, config interface{}) (error) {
+	c := reflect.ValueOf(config)
+	if c.Kind() == reflect.Ptr { c = c.Elem() }
+	if c.Type().Kind() != reflect.Struct {
+		return errors.New("config parameter is not a struct")
+	}
+
+	fileTmp, err := os.OpenFile(filename, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0644)
+	if err != nil { return err }
+	defer fileTmp.Close()
+
+	configLines := generateConfigLines ("", c)
+
+	for _, v := range configLines {
+		_, err = fileTmp.WriteString (v + "\n")
+		if err!= nil {
+			fileTmp.Close()
+			return err
+		}
+	}
+
+	return nil
 }
 
-//-------------------------------------------------------------------------------------------------
+//---------------------------------------------------------------------------------------------------------------------
+
+func generateConfigLines (keyprefix string, c reflect.Value) (result []string) {
+	var prefixTmp, valueTmp string
+	typeOfConfig := c.Type() 
+
+	for i := 0; i < c.NumField(); i++ {
+		fieldTmp := c.Field(i)
+		prefixTmp = keyprefix + typeOfConfig.Field(i).Name 
+		switch fieldTmp.Kind() {
+			case reflect.Uint16 : { 
+				valueTmp = strconv.FormatUint(fieldTmp.Uint() , 10)
+				result = append(result, prefixTmp + " = " + valueTmp ) 
+			}
+			case reflect.String : { 
+				result = append(result, prefixTmp + " = " + fieldTmp.String() ) 
+			}
+			case reflect.Struct : { 
+				result = append(result, generateConfigLines(prefixTmp + ".", fieldTmp)...)
+			}
+			// default not handled 
+		}
+	}
+
+	return result
+}
+
+//---------------------------------------------------------------------------------------------------------------------
+
+
+
+
+
--- simple_config_test.go
+++ simple_config_test.go
@@ -4,16 +4,40 @@ import (
 	"testing"
 )
 
+type DatabaseConfig struct {
+	Hostname string
+	Port     uint16
+	Name     string
+	Login    string
+	Password string
+}
+
+type ConfigType1 struct {
+	AppName		 string
+	Web struct {
+		Hostname string
+		Port     uint16
+	}
+	Database DatabaseConfig
+}
+
 //-------------------------------------------------------------------------------------------------
 
-func TestConfigRead(t *testing.T) {
+func Test_1_ConfigRead(t *testing.T) {
 	t.Errorf("TODO")
 }
 
 //-------------------------------------------------------------------------------------------------
 
-func TestConfigWrite(t *testing.T) {
-	t.Errorf("TODO")
+func Test_1_ConfigWrite(t *testing.T) {
+	config1 := ConfigType1{ AppName : "HelloWorld" }
+	config1.Web.Hostname = "localhost"
+	config1.Web.Port = 9000
+	dbconfig := DatabaseConfig{Hostname: "127.0.0.1", Port: 9000, Name: "testdb", 
+		Login: "guest", Password: "guest"}
+	config1.Database = dbconfig
+	err := WriteConfig("tests_output/config1.txt", config1)
+	if err != nil {	t.Errorf(err.Error()) }
 }
 
 //-------------------------------------------------------------------------------------------------
